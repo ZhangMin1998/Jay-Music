1a219ca1f5542e8e51255379d832187a9ed5a5a1
zhangmin <1464253423@qq.com>
2023-05-07 01:45:07 +0800

    自定义歌单组件playlist

---
+++ miniprogram/components/playlist/playlist.js
@@ -0,0 +1,25 @@
+// components/playlist/playlist.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    playlist: {
+      type: Object,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    _count: 0,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
